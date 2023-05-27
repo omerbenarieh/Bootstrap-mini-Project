0055f677792869274dce95dabda8877a20c855ce
omerbenarieh <omerrbenarieh@gmail.com>
2023-05-27 21:47:55 +0300

    cards

--- script.js
+++ script.js
@@ -1,78 +1,68 @@
 // Make sure that document is ready before manipulating the DOM.
 $('#board-container').hide();
-
-const options = {
-  // starting time
-  startValue: 0,
-
-  // max time
-  maxValue: 60,
-
-  // in milliseconds
-  counter: 1000,
-
-  // should the circles styling alters according to progress level
-  triggerPercentage: false,
-
-  // timer, progress or manual
-
-  type: 'timer',
-
-  // the width of the dial
-
-  dialWidth: 5,
-
-  // font size
-
-  fontSize: '20px',
-
-  // font color
-
-  fontColor: 'rgb(135, 206, 235)',
-
-  // skin name
-
-  skin: '',
-
-  // the size of the circle
-
-  size: '150px',
-};
-
 $(document).ready(function () {
-  /*
-  Stages:
-
-  2) Start a timer in game-board.
-  
-  */
+  // Varibales declarations
   const startBtn = $('#start');
   const userName = $('#username');
   const pairs = $('#pairs');
-
   const exitBtn = $('#exit');
-
   const loginContainer = $('#login-container');
   const boardContainer = $('#board-container');
 
-  // Start Game.
-  // 1) Show username on screen
+  // Start Game Button
   startBtn.click(e => {
     e.preventDefault();
     if (userName.val().length === 0 || pairs.val().length === 0) {
       return alert('Please enter a valid name and valid number of Pairs.');
     }
-    loginContainer.slideUp('slow');
+    startTimer(updateTimer);
+    loginContainer.slideUp('fast');
     boardContainer.slideDown('slow');
     $('#name').text(`Hello ${userName.val()}!`);
-    $('.myInstance').Circlebar(options);
     clearInputs();
   });
 
+  // Exit Game Button
   exitBtn.click(e => {
     e.preventDefault();
-    boardContainer.slideUp('slow');
+    boardContainer.slideUp('fast');
     loginContainer.slideDown('slow');
+    resetTimer();
     clearInputs();
   });
+
+  // Handler Functions
+  const clearInputs = () => {
+    userName.val('');
+    pairs.val('');
+  };
+
+  let timerInterval;
+  let totalSeconds = 0;
+
+  const startTimer = () => {
+    timerInterval = setInterval(updateTimer, 1000);
+  };
+
+  const stopTimer = () => {
+    clearInterval(timerInterval);
+  };
+
+  const resetTimer = () => {
+    stopTimer();
+    totalSeconds = 0;
+    updateTimer();
+  };
+
+  const updateTimer = () => {
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = totalSeconds % 60;
+    const formattedTime = formatTime(minutes) + ':' + formatTime(seconds);
+    $('#clock').text(formattedTime);
+    totalSeconds++;
+  };
+
+  const formatTime = time => {
+    return time < 10 ? '0' + time : time;
+  };
 });
